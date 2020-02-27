fc80d10c5525847e80dee83889fe422665693ca6
danielcont <danilla@123>
2020-02-27 13:27:36 -0600

    Diseño y funcionalidad de botones

--- app/src/main/java/com/example/odontobank/LoginActivity.java
+++ app/src/main/java/com/example/odontobank/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.odontobank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Botón para registrar
+        TextView nuevo = findViewById(R.id.registrar);
+        nuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, Nuevo.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/odontobank/MainActivity.java
+++ app/src/main/java/com/example/odontobank/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.odontobank;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button boton_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Botón para Ingresar
+        boton_login = findViewById(R.id.loginButton);
+        boton_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+        // Botón para registrar
+        TextView nuevo = findViewById(R.id.registrar);
+        nuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Nuevo.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
