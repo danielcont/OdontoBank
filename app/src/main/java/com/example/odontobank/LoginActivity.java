00df7109ca941deb407057dcd4cde684fe4de9d3
danielcont <danilla@123>
2020-02-27 13:47:04 -0600

    Botón de regreso

--- app/src/main/java/com/example/odontobank/LoginActivity.java
+++ app/src/main/java/com/example/odontobank/LoginActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    ImageView back_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +26,14 @@ public class LoginActivity extends AppCompatActivity {
             }
 
         });
+
+        // Botón de regresar
+        back_button = findViewById(R.id.backButton);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
