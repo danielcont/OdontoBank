e8e3159a63ceeaee872f49b55c07ca89b187bfdb
danielcont <contrerasgz.daniel@gmail.com>
2020-03-15 01:44:15 -0600

    Se obtiene la ubicación del dispositivo

--- app/src/main/java/com/example/odontobank/Inicio.java
+++ app/src/main/java/com/example/odontobank/Inicio.java
@@ -1,8 +1,12 @@
 package com.example.odontobank;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -16,5 +20,17 @@ public class Inicio extends AppCompatActivity {
         setContentView(R.layout.activity_inicio);
 
         mAuth = FirebaseAuth.getInstance();
+
+        // Se pide la ubicación
+        ActivityCompat.requestPermissions(Inicio.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+
+        MyLocation g = new MyLocation(getApplicationContext());
+        Location l = g.getLocation();
+        if(l != null) {
+            double lat = l.getLatitude();
+            double lon = l.getLongitude();
+            Toast.makeText(getApplicationContext(), "LAT: " + lat + "\n LON: " + lon, Toast.LENGTH_LONG).show();
+        }
     }
+
 }
