0e8135a9dceaee7d6ce010e8ce30a9f202e28204
danielcont <contrerasgz.daniel@gmail.com>
2020-05-23 05:23:55 -0500

    Interfaz de MisPublicaciones terminada con mejor diseño de CardView

--- app/src/main/java/com/example/odontobank/MisPublicaciones.java
+++ app/src/main/java/com/example/odontobank/MisPublicaciones.java
@@ -7,32 +7,40 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.example.odontobank.Model.Publicacion;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MisPublicaciones extends AppCompatActivity implements RecyclerViewAdapter.OnPublicacionListener {
 
     private List<Publicacion> mis_publicaciones;
     RecyclerView recyclerView;
     private Context context;
+    private ImageView button;
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
     private FirebaseUser fuser;
+    private DocumentReference usuRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +51,14 @@ public class MisPublicaciones extends AppCompatActivity implements RecyclerViewA
         fuser = mAuth.getCurrentUser();
 
         db = FirebaseFirestore.getInstance();
+
+        button = findViewById(R.id.misBackButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView_publicaciones);
         recyclerView.setHasFixedSize(true);
@@ -55,7 +71,8 @@ public class MisPublicaciones extends AppCompatActivity implements RecyclerViewA
     private void agregarDatos() {
         final String id = fuser.getUid();
 
-        db.collection("publicacion")
+        db.collection("publicaciones")
+                .orderBy("uid", Query.Direction.ASCENDING)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -65,7 +82,31 @@ public class MisPublicaciones extends AppCompatActivity implements RecyclerViewA
 
                             for(QueryDocumentSnapshot doc: task.getResult()) {
                                 if(doc.get("uid").equals(id)) {
+                                    // poner dato por dato a mis publicaciones
+                                    // PENSAR COMO HACER EL SORT
+                                    double lat = Double.parseDouble(doc.get("latitud").toString());
+                                    double lon = Double.parseDouble(doc.get("longitud").toString());
+                                    Geocoder geocoder = new Geocoder(MisPublicaciones.this);
+                                    String city = "";
+
+                                    try {
+                                        List<Address>addresses = geocoder.getFromLocation(lat, lon,1);
+                                        if (geocoder.isPresent()) {
+                                            if (addresses.size()>0) {
+                                                Address returnAddress = addresses.get(0);
+
+                                                city = returnAddress.getLocality();
+
+                                            }
+                                        } else {
+
+                                        }
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+
                                     Publicacion publicacion = doc.toObject(Publicacion.class);
+                                    publicacion.setCiudad(city);
                                     mis_publicaciones.add(publicacion);
                                 }
                             }
@@ -91,8 +132,9 @@ public class MisPublicaciones extends AppCompatActivity implements RecyclerViewA
         mis_publicaciones.get(position);
 
         Intent viewMisPublicaciones = new Intent(this, Informacion.class);
-        //viewMisPublicaciones.putExtra("publicacion", publicacion);
+        viewMisPublicaciones.putExtra("publicacion", publicacion);
         startActivity(viewMisPublicaciones);
     }
+
 
 }
--- app/src/main/java/com/example/odontobank/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/odontobank/RecyclerViewAdapter.java
@@ -1,21 +1,23 @@
 package com.example.odontobank;
 
+import android.graphics.Color;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.bumptech.glide.Glide;
-import com.example.odontobank.Model.Publicacion;
+//import com.example.odontobank.Model.Publicacion;
 
 import java.util.List;
 
-import de.hdodenhof.circleimageview.CircleImageView;
-
 class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.PublicacionViewHolder> {
+    CardView cardView;
+    ImageView imagenAtencion;
 
     private final List<Publicacion> arrayPublicacion;
     private OnPublicacionListener mOnPublicacionListener;
@@ -25,28 +27,41 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.Publi
         this.mOnPublicacionListener = onPublicacionListener;
     }
 
-    public class PublicacionViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        CardView cardView;
-        TextView nombre;
-        CircleImageView Imagencard;
-        TextView atencion;
-        TextView ubicacion_publicacion;
-        OnPublicacionListener onPublicacionListener;
+    @Override
+    public void onBindViewHolder(PublicacionViewHolder holder, int position) {
+        holder.nombreEstudiante.setText(arrayPublicacion.get(position).getNombre());
+        holder.atencionMedica.setText(arrayPublicacion.get(position).getAtencion_medica());
+        holder.ubicacion_publicacion.setText(arrayPublicacion.get(position).getCiudad());
 
-        PublicacionViewHolder(View itemView, OnPublicacionListener onPublicacionListener) {
-            super(itemView);
-            cardView = itemView.findViewById(R.id.cardView);
-            nombre = itemView.findViewById(R.id.cvNombre);
-            Imagencard = itemView.findViewById(R.id.cvImagen);
-            atencion = itemView.findViewById(R.id.cvAtencionMedica);
-            ubicacion_publicacion = itemView.findViewById(R.id.cvUbicacion);
-            this.onPublicacionListener = onPublicacionListener;
-            itemView.setOnClickListener(this);
+        if(arrayPublicacion.get(position).getAtencion_medica().equals("Endodoncia")) {
+            imagenAtencion.setImageResource(R.drawable.endodoncia);
+        } else if(arrayPublicacion.get(position).getAtencion_medica().equals("Cirugía Oral")) {
+            imagenAtencion.setImageResource(R.drawable.cigugia_oral);
+        } else if(arrayPublicacion.get(position).getAtencion_medica().equals("Odontología Preventiva")) {
+            imagenAtencion.setImageResource(R.drawable.odontologia_preventiva);
+        } else if(arrayPublicacion.get(position).getAtencion_medica().equals("Periodoncia")) {
+            imagenAtencion.setImageResource(R.drawable.periodoncia);
+        } else if(arrayPublicacion.get(position).getAtencion_medica().equals("Terapéutica Dental")) {
+            imagenAtencion.setImageResource(R.drawable.terapeutica_dental);
+        } else if(arrayPublicacion.get(position).getAtencion_medica().equals("Prótesis Dental")) {
+            imagenAtencion.setImageResource(R.drawable.protesis_dental);
+        } else if(arrayPublicacion.get(position).getAtencion_medica().equals("Odontología Infantil")) {
+            imagenAtencion.setImageResource(R.drawable.odontologia_infantil);
+        } else if(arrayPublicacion.get(position).getAtencion_medica().equals("Dolor Orofacial")) {
+            imagenAtencion.setImageResource(R.drawable.dolor_orofacial);
+        } else if(arrayPublicacion.get(position).getAtencion_medica().equals("Ortodoncia")) {
+            imagenAtencion.setImageResource(R.drawable.ortodoncia);
+        } else if(arrayPublicacion.get(position).getAtencion_medica().equals("Cariología")) {
+            imagenAtencion.setImageResource(R.drawable.cariologia);
+        } else if(arrayPublicacion.get(position).getAtencion_medica().equals("Implatología")) {
+            imagenAtencion.setImageResource(R.drawable.implantologia);
         }
 
-        @Override
-        public void onClick(View v) {
-            onPublicacionListener.onPublicacionClick(getAdapterPosition(), arrayPublicacion.get(getAdapterPosition()));
+        if(!arrayPublicacion.get(position).getActivado().equals("1")) {
+            holder.atencionMedica.setTextColor(Color.argb(85, 37, 58, 75));
+            holder.nombreEstudiante.setTextColor(Color.argb(85, 37, 58, 75));
+            imagenAtencion.setAlpha(0.4f);
+            holder.ubicacion_publicacion.setTextColor(Color.argb(85, 242, 58, 95));
         }
 
     }
@@ -59,14 +74,27 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.Publi
         return cvh;
     }
 
-    @Override
-    public void onBindViewHolder(PublicacionViewHolder holder, int position) {
-        holder.nombre.setText(arrayPublicacion.get(position).getNombre_estudiante());
-        holder.atencion.setText(arrayPublicacion.get(position).getAtencion_medica());
+    public class PublicacionViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView nombreEstudiante;
+        TextView atencionMedica;
+        TextView ubicacion_publicacion;
+        OnPublicacionListener onPublicacionListener;
 
-        Glide.with(holder.itemView.getContext())
-                .load(arrayPublicacion.get(position).getUid_estudiante())
-                .into(holder.Imagencard);
+        PublicacionViewHolder(View itemView, OnPublicacionListener onPublicacionListener) {
+            super(itemView);
+            cardView = itemView.findViewById(R.id.cardView);
+            nombreEstudiante = itemView.findViewById(R.id.cvNombre);
+            atencionMedica = itemView.findViewById(R.id.cvAtencionMedica);
+            ubicacion_publicacion = itemView.findViewById(R.id.cvUbicacion);
+            imagenAtencion = itemView.findViewById(R.id.imgAtencion);
+            this.onPublicacionListener = onPublicacionListener;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onPublicacionListener.onPublicacionClick(getAdapterPosition(), arrayPublicacion.get(getAdapterPosition()));
+        }
 
     }
 
@@ -82,5 +110,134 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.Publi
 
     public interface OnPublicacionListener {
         void onPublicacionClick(int position, Publicacion publicacion_click);
+    }
+
+}
+
+class Publicacion implements Parcelable {
+
+    public static final Creator<Publicacion> CREATOR = new Creator<Publicacion>() {
+        @Override
+        public Publicacion createFromParcel(Parcel in) {
+            return new Publicacion(in);
+        }
+
+        @Override
+        public Publicacion[] newArray(int size) {
+            return new Publicacion[size];
+        }
+    };
+    String uid;
+    String nombre;
+    String atencion_medica;
+    String contacto;
+    String latitud;
+    String longitud;
+    String activado;
+    String ciudad;
+
+    protected Publicacion(Parcel in) {
+        uid = in.readString();
+        nombre = in.readString();
+        atencion_medica = in.readString();
+        contacto = in.readString();
+        latitud = in.readString();
+        longitud = in.readString();
+        activado = in.readString();
+        ciudad = in.readString();
+    }
+
+    public Publicacion(String uid, String nombre, String atencion_medica, String contacto, String latitud, String longitud, String activado, String ciudad) {
+        this.uid = uid;
+        this.nombre = nombre;
+        this.atencion_medica = atencion_medica;
+        this.contacto = contacto;
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.activado = activado;
+        this.ciudad = ciudad;
+    }
+
+    public Publicacion() {}
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(uid);
+        dest.writeString(nombre);
+        dest.writeString(atencion_medica);
+        dest.writeString(contacto);
+        dest.writeString(latitud);
+        dest.writeString(longitud);
+        dest.writeString(activado);
+        dest.writeString(ciudad);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAtencion_medica() {
+        return atencion_medica;
+    }
+
+    public void setAtencion_medica(String atencion_medica) {
+        this.atencion_medica = atencion_medica;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+
+    public String getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        this.latitud = latitud;
+    }
+
+    public String getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(String longitud) {
+        this.longitud = longitud;
+    }
+
+    public String getActivado() {
+        return activado;
+    }
+
+    public void setActivado(String activado) {
+        this.activado = activado;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
     }
 }
