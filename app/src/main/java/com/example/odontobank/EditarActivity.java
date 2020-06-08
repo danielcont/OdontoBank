c5758d622e6a9ebad04c6f7ce185a6715ee10917
danielcont <danilla@123>
2020-06-08 05:22:15 -0500

    Editar carrera del estudiante

--- app/src/main/java/com/example/odontobank/EditarActivity.java
+++ app/src/main/java/com/example/odontobank/EditarActivity.java
@@ -7,9 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -30,6 +32,7 @@ public class EditarActivity extends AppCompatActivity {
     private ImageView back_button;
     private EditText correo, nombre, apellido, escuela;
     private Button modificiar_boton;
+    private Spinner spinnerEditarPerfil;
 
     FirebaseAuth mAuth;
 
@@ -93,16 +96,28 @@ public class EditarActivity extends AppCompatActivity {
                 validar();
             }
         });
+
+
+
+        spinnerEditarPerfil = findViewById(R.id.spinnerPerfil);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.carreras, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerEditarPerfil.setAdapter(adapter);
+        //spinnerEditarPerfil.setOnItemSelectedListener(this);
+
+                int spinnerPosition = adapter.getPosition("Seleccionar Opción");
+        spinnerEditarPerfil.setSelection(spinnerPosition);
     }
 
     private void validar() {
         mAuth = FirebaseAuth.getInstance();
 
-        boolean nombre_validado = false, apellido_validado = false, escuela_validado = false;
+        boolean nombre_validado = false, apellido_validado = false, escuela_validado = false, carrera_validado = false;
 
         TextView nombre_text = (TextView)findViewById(R.id.eNombre_text);
         TextView apellido_text = (TextView)findViewById(R.id.eApellido_text);
         TextView escuela_text = (TextView)findViewById(R.id.eEscuela_text);
+        TextView jj = findViewById(R.id.textView220);
 
         String sCorreo = correo.getText().toString();
 
@@ -118,13 +133,20 @@ public class EditarActivity extends AppCompatActivity {
 
         if(sEscuela.length() != 0) { escuela_validado = true; escuela_text.setVisibility(View.INVISIBLE); } else { escuela_text.setVisibility(View.VISIBLE); }
 
-        if((nombre_validado && apellido_validado && escuela_validado) == true) {
+        if(!spinnerEditarPerfil.getSelectedItem().toString().equals(R.string.options)) {
+            carrera_validado = true;
+            jj.setVisibility(View.INVISIBLE);
+        } else {
+            escuela_text.setVisibility(View.VISIBLE);
+        }
+
+        if((nombre_validado && apellido_validado && escuela_validado && carrera_validado) == true) {
             Map<String, Object> datos = new HashMap<>();
             datos.put("apellido", sApellido);
             datos.put("escuela", sEscuela);
             datos.put("nombre", sNombre);
             datos.put("correo", sCorreo);
-
+            datos.put("carrera", spinnerEditarPerfil.getSelectedItem().toString());
 
             String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
             ref = db.document("estudiantes/" + uid);
